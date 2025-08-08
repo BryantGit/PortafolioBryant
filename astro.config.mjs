24f11cd0547e9e589bfd138deaec3f2ce4a19121
Bryant Ponce <bip_israel@live.com>
2025-08-07 21:51:27 -0600

    Cambio de fondo para portafolio.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,3 +7,22 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 });
+
+module.exports = {
+   theme: {
+    extend: {
+      animation: {
+        blob: "blob 25s infinite ease-in-out",
+      },
+      keyframes: {
+        blob: {
+          "0%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(20px, -30px) scale(1.05)" },
+          "66%": { transform: "translate(-20px, 10px) scale(0.95)" },
+          "100%": { transform: "translate(0px, 0px) scale(1)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
