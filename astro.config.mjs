9ce75b114824ac7ed989ced88a0d9dbc0588e4a4
Bryant Ponce <bip_israel@live.com>
2025-08-07 21:54:31 -0600

    Actualizacion de fondos

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,16 +9,16 @@ export default defineConfig({
 });
 
 module.exports = {
-   theme: {
+  theme: {
     extend: {
       animation: {
-        blob: "blob 25s infinite ease-in-out",
+        blob: "blob 35s infinite ease-in-out",
       },
       keyframes: {
         blob: {
           "0%": { transform: "translate(0px, 0px) scale(1)" },
-          "33%": { transform: "translate(20px, -30px) scale(1.05)" },
-          "66%": { transform: "translate(-20px, 10px) scale(0.95)" },
+          "33%": { transform: "translate(25px, -35px) scale(1.05)" },
+          "66%": { transform: "translate(-25px, 20px) scale(0.95)" },
           "100%": { transform: "translate(0px, 0px) scale(1)" },
         },
       },
