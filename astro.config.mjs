80af88c28fcb5cfb90c576b8f83bca6306197d2f
Bryant Ponce <bip_israel@live.com>
2025-08-08 10:28:03 -0600

    guardado version 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,3 @@ export default defineConfig({
   },
 });
 
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        blob: "blob 35s infinite ease-in-out",
-      },
-      keyframes: {
-        blob: {
-          "0%": { transform: "translate(0px, 0px) scale(1)" },
-          "33%": { transform: "translate(25px, -35px) scale(1.05)" },
-          "66%": { transform: "translate(-25px, 20px) scale(0.95)" },
-          "100%": { transform: "translate(0px, 0px) scale(1)" },
-        },
-      },
-    },
-  },
-  plugins: [],
-}
